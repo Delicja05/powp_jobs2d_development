c4419d7185dfa74f5843c7219e2883f880dbe589
pallineczka <paulina.stelmasiak97@gmail.com>
2019-06-26 13:30:41 +0200

    add test to history

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigure2OptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigure2OptionListener.java
@@ -3,7 +3,10 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.manager.CommandHistory;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.features.CommandsFeature;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigure2OptionListener implements ActionListener {
@@ -17,5 +20,9 @@ public class SelectTestFigure2OptionListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
+		if(!CommandHistory.logMode)
+			CommandHistory.addEntry(CommandsFeature.getDriverCommandManager().getCommandList(),CommandsFeature.getDriverCommandManager().getCommandName());
+		command.execute(driverManager.getCurrentDriver());
 	}
 }
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,7 +3,10 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.manager.CommandHistory;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.features.CommandsFeature;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -17,5 +20,9 @@ public class SelectTestFigureOptionListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
+		if(!CommandHistory.logMode)
+			CommandHistory.addEntry(CommandsFeature.getDriverCommandManager().getCommandList(),CommandsFeature.getDriverCommandManager().getCommandName());
+		command.execute(driverManager.getCurrentDriver());
 	}
 }
